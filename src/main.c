0aeca2f85e0779246033488d98b297ae1bb1d064
kjeller <kallestralman@gmail.com>
2020-02-16 20:22:21 +0100

    Added some debug prints and tested some functionality

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 /*
- * A FLISP VM used for testing if flispcc works correctly.
- * Not all instructions are implemented 
+ * This is a VM implementation for the FLISP 
+ * (FLexible Instructions Set Processor).
+ *
+ * flispvm used for testing if flispcc works correctly.
+ * Reads S19 file into memory and then executes starting from 
+ * address 20. There are many instructions in the FLISP ISA
+ * and not all are implemented here. Only the most common ones, that
+ * is used within the flispcc compiler.
+ *  
  * Author: Karl Strålman
  * Inspired by: https://justinmeiners.github.io/lc3-vm/
  */
@@ -8,9 +15,19 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "opcodes.h"
+#include "include/opcodes.h"
+
 #define RST_VECTOR 0xFF
 #define MEM_SIZE 256
+
+/* Reads bytes from s19 file to memory
+ * (Specifically uses S1 and S9 record fields)
+ * This file format may also be known as SRECORD, SREC, S19, S28, S37
+ * */
+int read_s19_file(FILE *file);
+
+/* Open filestream to a file*/
+int read_file(const char *image_path); 
 
 
 /* All registers in Flisp */
@@ -73,10 +90,97 @@ void update_flags(uint8_t r) {
 void unsigned_fcheck(uint8_t a, uint8_t b) {}
 void signed_fcheck(int8_t a, int8_t b) {}
 
-/* Reads bytes from s19 file to memory
- * (Specifically uses S1 and S9 record fields)
- * This file format may also be known as SRECORD, SREC, S19, S28, S37
- */
+/* Uses next byte in memory as operand.
+ * PC will then increase
+ * */
+int8_t op_arg() {
+  return mem_read(reg[R_PC]++);
+}
+
+int main(int argc, const char *argv[]) {
+  if(argc < 2) {
+    printf("Usage: flispvm [s19-file]\n");
+    return 1;
+  }
+
+  for(int i = 1; i < argc; i++) {
+    if(read_file(argv[i])) {
+      printf("Failed to load file: %s\n", argv[i]);
+      return 1;
+    }
+  }
+  
+  mem_print();
+
+  /* Initial fetch: Set PC from RST_VECTOR*/
+  //reg[R_PC] = mem_read(RST_VECTOR);
+  reg[R_PC] = 0x20;
+  reg[R_SP] = 0x20;
+  int alive = 1;
+  while(alive) {
+    printf("PC=%02X, SP=%02X\n", reg[R_PC], reg[R_SP]);
+
+    /* Fetch */
+    uint8_t op_code = mem_read(reg[R_PC]++);
+     
+    switch(op_code) {
+      case NOP:
+        break;
+      /* Load/Store */
+      case LDA_IM:
+        reg[R_A] = op_arg();
+        update_flags(reg[R_A]);
+        break;
+      case LDA_AB:
+        reg[R_A] = mem_read(op_arg());
+        update_flags(reg[R_A]);
+        break;
+      case LDA_NS:
+        reg[R_A] = mem_read(reg[R_SP] + op_arg());
+        update_flags(reg[R_A]);
+        break;
+      case LEASP_NS:
+        reg[R_SP] += op_arg();
+        break;
+      case STA_AB:
+        mem_write(op_arg(), reg[R_A]);
+        break;
+      
+      /* Integer arithmetic */
+      case ADDA_NS:
+        reg[R_A] += mem_read(op_arg() + reg[R_SP]);
+        break;
+      case DECA:
+        reg[R_A]--;
+        break;
+      
+      /* Stack operations */
+      case PSHA:
+        reg[R_SP] -= 1;
+        mem_write(reg[R_SP], reg[R_A]);
+        break;
+
+      /* Program flow */
+      case JSR_AB:
+        reg[R_SP] -= 1;
+        mem_write(reg[R_SP], reg[R_PC]);
+        reg[R_PC] = op_arg();
+        break;
+      case RTS:
+        reg[R_PC] = mem_read(reg[R_SP]++);
+        break;
+
+      /* Not implemented case or bad op code */
+      default:
+        alive = 0;
+        printf("Warning: Op code (%02X) not implemented\n", op_code);
+        printf("Stopped at: PC=%02X\n", reg[R_PC]);
+        break;
+    }
+  }
+}
+
+/* ===== File input functions below ===== */
 int read_s19_file(FILE *file) {
   char tmp[2]; //place to store S1 and finally overwrite S9
   
@@ -146,8 +250,6 @@ int read_s19_file(FILE *file) {
   return 0;   
 }
 
-
-/* Open filestream to a file*/
 int read_file(const char *image_path) {
   FILE *file = fopen(image_path, "rb");
   if(!file) return 0;
@@ -157,77 +259,4 @@ int read_file(const char *image_path) {
   return 0;
 }
 
-int main(int argc, const char *argv[]) {
-  if(argc < 2) {
-    printf("Usage: flispvm [s19-file]\n");
-    return 1;
-  }
 
-  for(int i = 1; i < argc; i++) {
-    if(read_file(argv[i])) {
-      printf("Failed to load file: %s\n", argv[i]);
-      return 1;
-    }
-  }
-  
-  mem_print();
-
-  /* Initial fetch: Set PC from RST_VECTOR*/
-  reg[R_PC] = mem_read(RST_VECTOR);
-
-  int alive = 1;
-  while(alive) {
-
-    /* Fetch */
-    uint8_t op_code = mem_read(reg[R_PC]++);
-    
-    /* Assumes 2nd byte. Not used by every instruction.
-     * It will refer to another instruction in cases 
-     * where instr is a one byte instruction,
-     *  and will therefore not be used. */
-    uint8_t op_arg = mem_read(reg[R_PC]);
-    
-    switch(op_code) {
-      /* LD */
-      case LDA_IM:
-        reg[R_A] = op_arg;
-        update_flags(reg[R_A]);
-        break;
-      case LDA_AB:
-        reg[R_A] = mem_read(op_arg);
-        update_flags(reg[R_A]);
-        break;
-      case LDA_NS:
-        reg[R_A] = mem_read(reg[R_SP] + op_arg);
-        update_flags(reg[R_A]);
-        break;
-      /* LEA*/
-      case LEASP_NS:
-        reg[R_SP] = op_arg + reg[R_SP];
-        break;
-      /* STA */
-      case STA_AB:
-        mem_write(op_arg, reg[R_A]);
-        break;
-      /* PSH */
-      case PSHA:
-        reg[R_SP] = reg[R_SP] - 1;
-        mem_write(reg[R_SP], reg[R_A]);
-        break;
-
-      case JSR_AB:
-        reg[R_SP] = reg[R_SP] - 1;
-        mem_write(reg[R_SP], reg[R_PC]);
-        reg[R_PC] = op_arg;
-        break;
-      case RTS:
-        reg[R_PC] = mem_read(reg[R_SP]++);
-        break;
-      /* Not implemented case or bad op code */
-      default:
-        alive = 0;
-        printf("Warning: Op code not implemented\n");
-        break;
-    }
-  }
-}
